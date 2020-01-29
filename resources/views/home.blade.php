c06b05ffc1fb265760d1a1c7ad433fb1adc252b0
sha-zha <adeline.servan@gmail.com>
2020-01-29 18:29:38 +0400

    lien boostrap sur app.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <a href="{{ route('ajout') }}">Créer un utilisateur</a>
                 </div>
             </div>
         </div>
